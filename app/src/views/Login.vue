315b9192098873178d70dfb663d3540027252005
Ninjapiraatti <tuoppil@gmail.com>
2021-03-20 21:12:01 +0200

    User fetched from DB is updated in frontend

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
-		{{ message }}
+		<a v-on:click="getUserData">{{ message }}</a>
+		<p>{{ user.name }}</p>
 	</div>
 </template>
 
@@ -9,7 +10,29 @@
 		name: 'Login',
 		data() {
 			return {
-				message: "This would be the login page."
+				message: "Click here",
+				user: {}
+			}
+		},
+		methods: {
+			getUserData: function() {
+				console.log("getUserData fired.");   
+				fetch('/user/cc7cd70e-bbe0-4a63-8bec-9c63f60a91c4', {method: 'GET'})
+				.then((response) => response.json())
+				.then(response => { 
+					console.log(response);
+					this.user = response;
+					this.$flashMessage.show({
+						type: 'success',
+						title: 'Successfully fetched data',
+						time: 1000
+					});
+				})    
+				.catch((errors) => {    
+					console.log("Could not get data");
+					console.log(errors);
+					//this.$router.push('/create'); 
+				})
 			}
 		}
 	}
