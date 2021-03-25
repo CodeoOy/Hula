3f554b924da4298fe1b75b00d6e884bf1453886c
Ninjapiraatti <tuoppil@gmail.com>
2021-03-25 16:32:33 +0200

    Moved components to src folder, implementing Bootstrap

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,14 @@
 <template>
-	<div>
+	<main>
+		<Header />
 		<FlashMessage position="right top" />
 		<h1>Rust Vue POC</h1>
 		<p class="routermessage"><router-view /></p>
-	</div>
+	</main>
 </template>
 
 <script>
+	import Header from './components/Header.vue' 
 	export default {
 		name: 'App',
 
@@ -15,6 +17,9 @@
 				variable: false,
 				currentpath: this.$router.currentRoute.value.path
 			}
-		}
+		},
+		components: {
+			'Header': Header,
+  		}
 	}
 </script>
---
+++ app/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+	<header class="p-3 bg-dark text-white">
+	<div class="container">
+		<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+		<a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+			<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+		</a>
+
+		<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+			<li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+		</ul>
+
+		<div class="text-end">
+			<button type="button" class="btn btn-outline-light me-2">Login</button>
+			<button type="button" class="btn btn-warning">Sign-up</button>
+		</div>
+		</div>
+	</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'Header'
+};
+</script>
