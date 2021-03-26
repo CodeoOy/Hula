11750ff596ff0494f7f63fd249cbe75054a2f0f6
Ninjapiraatti <tuoppil@gmail.com>
2021-03-26 12:18:46 +0200

    More tropical tweaks

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -3,16 +3,18 @@
 	<div class="container">
 		<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 		<a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
-			<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+			<!--<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>-->
+			<span class="font-heading font-heading--large">Tropical ERP</span>
 		</a>
 
 		<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-			<li><a href="#" class="nav-link px-2 font-heading font-heading--large">Tropical ERP</a></li>
+			<li><a href="#" class="nav-link px-2">Treefloof</a></li>
+			<li><a href="#" class="nav-link px-2">Bunno</a></li>
+			<li><a href="#" class="nav-link px-2">Cheeseboi</a></li>
 		</ul>
 
 		<div class="text-end">
-			<button type="button" class="btn btn-outline-dark me-2">Login</button>
-			<button type="button" class="btn btn-warning">Sign-up</button>
+			<button type="button" class="btn btn-warning">Yellow button</button>
 		</div>
 		</div>
 	</div>
