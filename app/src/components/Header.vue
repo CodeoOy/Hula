eacf8cedfcef99302942e0643e871f4687c023da
Ninjapiraatti <tuoppil@gmail.com>
2021-04-16 13:43:43 +0300

    Starting to bring in icons

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -14,7 +14,9 @@
 		</ul>
 
 		<div class="text-end">
-			<button type="button" class="btn btn-warning">Yellow button</button>
+			<svg class="icon" width="32" height="32" viewbox="0 0 32 32" fill="currentColor">
+				<path d="M11 6a3 3 0 11-6 0 3 3 0 016 0z"></path><path fill-rule="evenodd" d="M0 8a8 8 0 1116 0A8 8 0 010 8zm8-7a7 7 0 00-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 008 1z"></path>
+			</svg>
 		</div>
 		</div>
 	</div>
