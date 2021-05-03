79be515fe6e971fa5b360ae7567110b7ec5aadda
Ninjapiraatti <tuoppil@gmail.com>
2021-05-03 13:24:37 +0300

    Added name fields to register form

--- app/src/components/Register.vue
+++ app/src/components/Register.vue
@@ -1,13 +1,21 @@
 <template>
 	<form v-on:submit="register">
 		<div class="mb-3">
-			<label for="exampleInputUser" class="form-label">Email</label>
-			<input type="text" class="form-control" id="exampleInputUser" aria-describedby="emailHelp" name="email">
+			<label for="inputEmail" class="form-label">Email</label>
+			<input type="text" class="form-control" id="inputEmail" aria-describedby="emailHelp" name="email">
 			<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 		</div>
 		<div class="mb-3">
-			<label for="exampleInputPassword" class="form-label">Password</label>
-			<input type="password" class="form-control" id="exampleInputPassword" name="password_plain">
+			<label for="inputFirstName" class="form-label">First name</label>
+			<input type="text" class="form-control" id="inputFirstName" name="first_name">
+		</div>
+		<div class="mb-3">
+			<label for="inputLastName" class="form-label">Last name</label>
+			<input type="text" class="form-control" id="inputLastName" name="last_name">
+		</div>
+		<div class="mb-3">
+			<label for="inputPassword" class="form-label">Password</label>
+			<input type="password" class="form-control" id="inputPassword" name="password_plain">
 		</div>
 		<button type="submit" class="btn btn-gradient mb-1">Register</button>
 	</form>
@@ -27,11 +35,15 @@
 
 				e.preventDefault()    
 				let email = e.target.elements.email.value
+				let first_name = e.target.elements.first_name.value 
+				let last_name = e.target.elements.last_name.value 
 				let password_plain = e.target.elements.password_plain.value 
 				let getUserData = () => {   
 					let data = {    
 						"email": email,
-						"password_plain": password_plain
+						"password_plain": password_plain,
+						"first_name": first_name,
+						"last_name": last_name,
 					}
 					fetch('api/invitation', {method: 'POST', headers: {"Content-Type": "application/json"}, body: JSON.stringify(data)})
 					.then((response) => response.json())
