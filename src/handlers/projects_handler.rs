09da5e57537314e9bb00d83642aaa58705b898fd
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-05-04 09:43:04 +0300

    sorted

--- src/handlers/projects_handler.rs
+++ src/handlers/projects_handler.rs
@@ -1,5 +1,6 @@
 use actix_web::{error::BlockingError, web, HttpResponse};
 use diesel::{prelude::*, PgConnection};
+use serde::Deserialize;
 
 use crate::errors::ServiceError;
 use crate::models::projects::{Pool, Project};
@@ -9,7 +10,7 @@ pub async fn get_all_projects(
 ) -> Result<HttpResponse, ServiceError> {
 	// run diesel blocking code
 	println!("\nGetting all projects");
-	let res = web::block(move || query(pool)).await;
+	let res = web::block(move || query_all(pool)).await;
 
 	match res {
 		Ok(project) => Ok(HttpResponse::Ok().json(&project)),
@@ -20,7 +21,7 @@ pub async fn get_all_projects(
 	}
 }
 
-fn query(
+fn query_all(
 	pool: web::Data<Pool>,
 ) -> Result<Vec<Project>, crate::errors::ServiceError> {
 	use crate::schema::projects::dsl::{projects};
@@ -32,4 +33,44 @@ fn query(
 		return Ok(items);
 	}
 	Err(ServiceError::Empty)
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct QueryData {
+	pub id: String
+}
+
+pub async fn get_by_pid(
+	uuid_data: web::Json<QueryData>,
+	pool: web::Data<Pool>,
+) -> Result<HttpResponse, ServiceError> {
+	// run diesel blocking code
+	println!("\nGetting project by uuid");
+	let res = web::block(move || query_one(uuid_data.into_inner(), pool)).await;
+
+	match res {
+		Ok(project) => Ok(HttpResponse::Ok().json(&project)),
+		Err(err) => match err {
+			BlockingError::Error(service_error) => Err(service_error),
+			BlockingError::Canceled => Err(ServiceError::InternalServerError),
+		},
+	}
+}
+
+fn query_one(
+	uuid_data: QueryData,
+	pool: web::Data<Pool>,
+) -> Result<Project, crate::errors::ServiceError> {
+	use crate::schema::projects::dsl::{id, projects};
+	let conn: &PgConnection = &pool.get().unwrap();
+	let uuid_query = uuid::Uuid::parse_str(&uuid_data.id)?;
+	let mut items = projects
+        .filter(id.eq(uuid_query))
+		.load::<Project>(conn)?;
+	if let Some(project_res) = items.pop() {
+		println!("\nQuery successful.\n");
+		return Ok(project_res.into());
+	}
+	Err(ServiceError::Unauthorized)
 }
