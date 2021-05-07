9a90dfc6aeb8d02c57e1b8fed445e19acfe2f239
Ninjapiraatti <tuoppil@gmail.com>
2021-05-07 10:49:55 +0300

    Added some frontend components for the skill form

--- app/src/views/Profile.vue
+++ app/src/views/Profile.vue
@@ -4,6 +4,17 @@
 			<div class="modal-dialog">
 				<div class="modal-content p-3 rounded-2 content-box bg-dark text-light">
 					<div>
+						<h2>Add a skill</h2>
+						<div class="input-group">
+							<select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+								<option selected>Choose...</option>
+								<option value="1">One</option>
+								<option value="2">Two</option>
+								<option value="3">Three</option>
+							</select>
+							<input type="number" aria-label="Years" class="form-control">
+							<button class="btn btn-outline-secondary" type="button">Button</button>
+						</div>
 						<a href="#" v-on:click="editing_skills = false" class="btn btn-gradient">Done</a>
 					</div>
 				</div>
@@ -68,7 +79,8 @@
 					skillscopelevel_id: "e9becc32-0238-4561-b341-106de1c26048",
 					years: 10.0,
 					updated_by: "psi"
-				}
+				},
+				available_skills: {},
 			}
 		},
 		components: {
