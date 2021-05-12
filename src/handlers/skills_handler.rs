c718159552abefc08a23e37b17c3bc198c7ab520
Ninjapiraatti <tuoppil@gmail.com>
2021-05-12 11:07:41 +0300

    Skill created with hardcoded ids

--- src/handlers/skills_handler.rs
+++ src/handlers/skills_handler.rs
@@ -15,7 +15,7 @@ pub async fn create_skill(
 	skilldata: web::Json<SkillData>,
 	pool: web::Data<Pool>
 ) -> Result<HttpResponse, ServiceError> {
-	println!("Adding skill");
+	println!("Creating a skill");
 	let res = web::block(move || query_create(skilldata, pool)).await;
 	match res {
 		Ok(skill) => Ok(HttpResponse::Ok().json(&skill)),
@@ -32,13 +32,15 @@ fn query_create(
 ) -> Result<Skill, crate::errors::ServiceError> {
 	use crate::schema::skills::dsl::skills;
 	let conn: &PgConnection = &pool.get().unwrap();
+	println!("Connected to db");
 	let new_skill = Skill {
 		id: uuid::Uuid::new_v4(),
 		label: skilldata.label.clone(),
-		skillcategory_id: uuid::Uuid::new_v4(),
-		skillscope_id: uuid::Uuid::new_v4(),
+		skillcategory_id: uuid::Uuid::parse_str("e9becc32-0238-4561-b341-106de1c26041")?, // Update to proper value
+		skillscope_id: uuid::Uuid::parse_str("e9becc32-0238-4561-b341-106de1c26043")?, // Update to proper value
 		updated_by: skilldata.email.clone(),
 	};
+	println!("Inserting data");
 	let rows_inserted = diesel::insert_into(skills)
 		.values(&new_skill)
 		.get_result::<Skill>(conn);
