90a0438a4cc73fb2dc2fe55ff18c3c050b6d6327
Ninjapiraatti <tuoppil@gmail.com>
2021-05-18 20:34:23 +0300

    Working on frontend for skill addition

--- app/src/components/Modal.vue
+++ app/src/components/Modal.vue
@@ -3,7 +3,7 @@
 		<div class="modal-dialog">
 			<div class="modal-content p-3 rounded-2 content-box bg-dark text-light">
 				<div>
-					<h2>Modal title</h2>
+					<h2>{{ modalTitle }}</h2>
 					<slot></slot>
 				</div>
 			</div>
@@ -15,27 +15,13 @@
 export default {
 	name: 'SkillScope',
 	props: {	
-		user: {},
 		showModal: false,
+		modalTitle: '',
 	},
 	data() {
 		return {
 			errorsPresent: false
 		};
-	},
-	methods: {
-		onSubmit: function() {
-			if (this.user.lastname === '') {
-				this.errorsPresent = true;
-				this.$flashMessage.show({
-					type: 'error',
-					title: 'Please fill out name.',
-					time: 1000
-				});
-			} else {
-				this.$emit('formsent', this.user);
-			}
-		},
 	}
 };
 </script>
--- app/src/views/Admin.vue
+++ app/src/views/Admin.vue
@@ -1,13 +1,16 @@
 <template>
 	<div class="container mt-4">
-		<Modal :showmodal='true'>
+		<Modal :showModal="showModal" :modalTitle="formTitle">
 			<Skill />
 		</Modal>
 		<div class="row gx-4">
 			<div class="col-md-4">
 				<div class="p-3 mb-4 rounded-2 content-box bg-dark text-light">
 					<h1>Welcome {{ this.$store.state.loggeduser.firstname }}!</h1>
-					<p><a href="#" v-on:click="showModal = true">Add skill</a></p>
+					<p><a href="#" v-on:click="showModal = true, formTitle = 'New Skill'">Add skill</a></p>
+					<p><a href="#" v-on:click="showModal = true, formTitle = 'New Scope'">Add scope</a></p>
+					<p><a href="#" v-on:click="showModal = true, formTitle = 'New Category'">Add category</a></p>
+					<p><a href="#" v-on:click="showModal = true, formTitle = 'New Level'">Add level</a></p>
 				</div>
 			</div>
 			<div class="col-md-8">
@@ -27,7 +30,8 @@
 		name: 'Admin',
 		data() {
 			return {
-				editing_skills: false,
+				showModal: false,
+				formTitle: '',
 				skilldata: {
 					label: '',
 					category_id: '',
