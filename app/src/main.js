27b28d862b1cff1dd709af7bc284321cfb370bf3
Tuomas Louekari <tuoppil@gmail.com>
2021-06-10 13:55:24 +0300

    Made a horrible callback hell but it works

--- app/src/main.js
+++ app/src/main.js
@@ -32,32 +32,40 @@ const store = createStore({
 			state.projects = data
 		},
 		setChosenProject (state, data) {
-			var project = state.projects.find(x => x.id == data);
-			fetch(`/api/projectneeds/${project.id}`, {
+			fetch(`/api/projects/${data}`, {
 				method: 'GET',
 				headers: {"Content-Type": "application/json"},
 				credentials: 'include'
 			})
 			.then((response) => response.json())
-			.then((response) => {
-				project.needs = response
-				project.needs.forEach(function (projectneed) {
-					fetch(`/api/projectskills/${projectneed.id}`, {
-						method: 'GET',
-						headers: {"Content-Type": "application/json"},
-						credentials: 'include'
-					})
-					.then((response) => response.json())
-					.then((response) => {
-						projectneed.skills = response
-					})
-				});
-			})
-			.then((response) => {
-				console.log("Project from state:")
-				console.log(project)
-				localStorage.setItem('chosenproject', JSON.stringify(project));
-				state.chosenproject = project
+				.then((response) => {
+				var project = response
+				fetch(`/api/projectneeds/${data}`, {
+					method: 'GET',
+					headers: {"Content-Type": "application/json"},
+					credentials: 'include'
+				})
+				.then((response) => response.json())
+				.then((response) => {
+					project.needs = response
+					project.needs.forEach(function (projectneed) {
+						fetch(`/api/projectskills/${projectneed.id}`, {
+							method: 'GET',
+							headers: {"Content-Type": "application/json"},
+							credentials: 'include'
+						})
+						.then((response) => response.json())
+						.then((response) => {
+							projectneed.skills = response
+						})
+					});
+				})
+				.then((response) => {
+					console.log("Project from state:")
+					console.log(project)
+					localStorage.setItem('chosenproject', JSON.stringify(project));
+					state.chosenproject = project
+				})
 			})
 		},
 		resetChosenProject (state) {
