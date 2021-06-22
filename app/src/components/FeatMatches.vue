458ca2eb37cd8812e62b5376f77cb43b5f720a68
Tuomas Louekari <tuoppil@gmail.com>
2021-06-22 07:07:57 +0300

    Restructured match content to its own component

--- app/src/components/FeatMatches.vue
+++ app/src/components/FeatMatches.vue
@@ -3,30 +3,6 @@
 		<VModal :modalTitle="'Match'" :modalID="'match'">
 			<MatchContent :match="currentMatch"/>
 		</VModal>
-		<div class="modal fade" v-bind:class="{ 'show db': show, '': !show }">
-			<div class="modal-dialog">
-				<div class="modal-content p-3 rounded-2 content-box bg-dark text-light">
-					<h2>Match</h2>
-					<table class="table table-dark table-striped text-light">
-						<thead>
-							<tr>
-								<th></th>
-								<th scope="col">{{ currentMatch.projectname }}</th>
-								<th scope="col">{{ currentMatch.firstname }} {{ currentMatch.lastname }}</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>{{ currentMatch.skillname }}</td>
-								<td>{{ currentMatch.required_minyears }}</td>
-								<td>{{ currentMatch.user_years }}</td>
-							</tr>
-						</tbody>
-					</table>
-					<button class="btn btn-gradient" v-on:click="show = false">Close</button>
-				</div>
-			</div>
-		</div>
 		<ul class="matches">
 			<li class="match" v-for="match in matches" :key="match.projectname">
 				<a href="#" data-bs-toggle="modal" data-bs-target="#hulaModalmatch" v-on:click="currentMatch = match">
