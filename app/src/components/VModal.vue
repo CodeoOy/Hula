5e5e3e866201201e3a6db95e299ff067ba66d375
Tuomas Louekari <tuoppil@gmail.com>
2021-07-05 08:57:15 +0300

    Removed the close icon from login

--- app/src/components/VModal.vue
+++ app/src/components/VModal.vue
@@ -2,7 +2,7 @@
 	<div class="modal fade" :id="'hulaModal' + modalID" :data-bs-backdrop="modalStatic ? 'static' : null">
 		<div class="modal-dialog">
 			<div class="modal-content rounded-2 content-box bg-dark text-light">
-				<div class="modal-header">
+				<div class="modal-header" v-if="modalStatic == false">
 					<h2 class="modal-title">{{ modalTitle }}</h2>
 					<button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
