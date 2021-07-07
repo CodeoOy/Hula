d0fc2024ca57d134b6a6b8f5504de6839ac8e611
Tuomas Louekari <tuoppil@gmail.com>
2021-07-07 15:55:29 +0300

    Trying to fix the timestamp again

--- app/src/forms/FormProjectNeed.vue
+++ app/src/forms/FormProjectNeed.vue
@@ -2,12 +2,11 @@
 	<v-form v-on:submit="createUpdateProjectNeed">
 		<h2 v-if="'id' in this.chosenNeed">{{ chosenNeed.id }}</h2>
 		<h2 v-else>New need</h2>
-		{{ chosenNeed }}
 		<div class="mb-2">
 			<label class="form-label">How many pros for this need?</label>
 			<error-message name="users" class="error"></error-message>
 			<v-field
-				v-model.number="chosenNeed.count_of_users"
+				v-model.number="queryData.count_of_users"
 				:rules="isRequired"
 				as="input"
 				type="number"
@@ -18,10 +17,10 @@
 		</div>
 		<div class="mb-2">
 			<label class="form-label">When does this need start?</label>
-			{{ chosenNeed.begin_time }}<br />
+			{{ queryData.begin_time }}<br />
 			<error-message name="begintime" class="error"></error-message>
 			<v-field
-				v-model="chosenNeed.begin_time"
+				v-model="queryData.begin_time"
 				:rules="isRequired"
 				as="input"
 				type="date"
@@ -32,10 +31,10 @@
 		</div>
 		<div class="mb-2">
 			<label class="form-label">When does this need end?</label>
-			{{ chosenNeed.end_time }}<br />
+			{{ queryData.end_time }}<br />
 			<error-message name="endtime" class="error"></error-message>
 			<v-field
-				v-model="chosenNeed.end_time"
+				v-model="queryData.end_time"
 				as="input"
 				type="date"
 				name="endtime"
@@ -47,7 +46,7 @@
 			<label class="form-label">At what percentage?</label>
 			<error-message name="percentage" class="error"></error-message>
 			<v-field
-				v-model.number="chosenNeed.percentage"
+				v-model.number="queryData.percentage"
 				:rules="isRequired"
 				as="input"
 				type="number"
@@ -76,20 +75,22 @@ export default {
 		createUpdateProjectNeed() {
 			let chosenMethod = 'POST'
 			let fetchPath = '/api/projectneeds'
-			if ('id' in this.chosenNeed) {
+			if ('id' in this.queryData) {
 				chosenMethod = 'PUT'
-				fetchPath = '/api/projectneeds/' + this.chosenNeed.id
+				fetchPath = '/api/projectneeds/' + this.queryData.id
 			} else {
-				this.chosenNeed.id = '00000000-0000-0000-0000-e033a6751fca'
+				this.queryData.id = '00000000-0000-0000-0000-e033a6751fca'
 			}
-			delete this.chosenNeed.skills
-			this.chosenNeed.begin_time = `${this.chosenNeed.begin_time}T00:00:00` // TODO: This breaks the api call if user doesn't change the dates
-			this.chosenNeed.end_time = `${this.chosenNeed.end_time}T00:00:00` // So maybe use moment.js?
+			delete this.queryData.skills
+			this.queryData.begin_time = `${this.queryData.begin_time}T00:00:00` 
+			console.log(this.queryData.begin_time)// TODO: This breaks the api call if user doesn't change the dates
+			this.queryData.end_time = `${this.queryData.end_time}T00:00:00` // So maybe use moment.js?
+			console.log(this.queryData.begin_time)
 			fetch(fetchPath, {
 				method: chosenMethod,
 				headers: {"Content-Type": "application/json"},
 				credentials: 'include',
-				body: JSON.stringify(this.chosenNeed)
+				body: JSON.stringify(this.queryData)
 			})
 			.then(() => {
 				this.$emit('formSent')
@@ -102,5 +103,18 @@ export default {
 	props: {
 		chosenNeed: {}
 	},
+	computed: {
+		queryData: {
+			get () {
+				var tempBeginTime = this.chosenNeed.begin_time.toString().replace('T00:00:00', '') 
+				var tempEndTime = this.chosenNeed.end_time.toString().replace('T00:00:00', '') 
+				this.chosenNeed.begin_time = tempBeginTime
+				if (tempEndTime) {
+					this.chosenNeed.end_time = tempEndTime
+				}
+				return this.chosenNeed
+			}
+		}
+	}
 };
 </script>
