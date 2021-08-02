511febc9e2f112952c7be020eca5ff3fa74a9ae5
Tuomas Louekari <tuoppil@gmail.com>
2021-08-02 07:19:38 +0300

    Working on forgotten/unset password

---
+++ app/src/forms/FormResetPassword.vue
@@ -0,0 +1,57 @@
+<template>
+	<v-form v-on:submit="inviteUser">
+		<div class="mb-2">
+			<label class="form-label">Password</label>
+			<error-message name="password" class="error"></error-message>
+			<v-field
+				v-model="formData.password"
+				:rules="isRequired"
+				as="input"
+				type="text"
+				name="password"
+				class="form-control"
+				aria-label="password"
+			></v-field>
+		</div>
+		<div class="mb-2">
+			<label class="form-label">Password again</label>
+			<error-message name="passwordAgain" class="error"></error-message>
+			<v-field
+				v-model="formData.passwordAgain"
+				:rules="isRequired"
+				as="input"
+				type="text"
+				name="passwordAgain"
+				class="form-control"
+				aria-label="password"
+			></v-field>
+		</div>
+		<button type="submit" class="btn btn-gradient mb-1">Submit</button>
+	</v-form>
+</template>
+
+<script>
+import { Field, Form, ErrorMessage } from 'vee-validate';
+export default {
+	name: 'FormResetPassword',
+	data() {
+		return {
+			user: {},
+			formData: {
+				password: '',
+				passwordAgain: '',
+			}
+		}
+	},
+	components: {
+		'VForm': Form,
+		'VField': Field,
+		ErrorMessage
+	},
+	methods: {
+		isRequired(value) {
+			return value ? true : 'This field is required';
+		},
+	}
+}
+</script>
