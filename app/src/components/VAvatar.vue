59473cefa4b36a9b4bf264a553a3af63d04f3b08
Tuomas Louekari <tuoppil@gmail.com>
2021-08-05 15:02:06 +0300

    Tweaking the tinting function

--- app/src/components/VAvatar.vue
+++ app/src/components/VAvatar.vue
@@ -48,9 +48,9 @@ export default {
             var r = parseInt(colorhex.substr(1, 2), 16);
             var g = parseInt(colorhex.substr(3, 2), 16);
             var b = parseInt(colorhex.substr(5, 2), 16);
-            this.r = r + 166 / 1.5
-            this.g = g + 13 / 10
-            this.b = b + 112 / 2
+            this.r = (r + 166) / 1.5
+            this.g = (g + 13) / 10
+            this.b = (b + 112) / 2
             return `rgba(${this.r}, ${this.g}, ${this.b}, 0.9)`
         },
         /*
