f14068c3e751fdc45a6e0becc3e413962e9361af
Tuomas Louekari <tuoppil@gmail.com>
2021-08-20 06:48:49 +0300

    Refactored a bit to avoid too much calculation

--- app/src/components/ResultsPros.vue
+++ app/src/components/ResultsPros.vue
@@ -3,7 +3,7 @@
 		<VModal :modalTitle="'Match'" :modalID="'match'">
 			<MatchContent :chosenMatch="currentMatch"/>
 		</VModal>
-		{{ skills }}
+		{{ project }}
 		<!-- <h2 v-if="project.name">Pro search results for {{ project.name }}</h2> -->
 		<transition name="fadeHeight">
 			<table v-if="matches" class="table table-dark table-striped text-light">
@@ -11,24 +11,24 @@
 					<tr>
 						<th scope="col"></th>
 						<th scope="col">Name</th>
-						<th scope="col">Has mandatory skills</th>
+						<th @click="sort('hasMandatory')" scope="col">Has mandatory skills</th>
 						<th scope="col">Matched skills</th>
 						<th scope="col">Tier</th>
 						<th scope="col">Available?</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr v-for="(user, index) in users" :key="user.user_first_name" :class="`tier tier--${tier(user)}`">
-						<th scope="row" class="tier__index"><span :class="`tier__ball tier__ball--${tier(user)}`" :style="`zIndex: ${index}`">{{ index + 1 }}</span></th>
+					<tr v-for="(user, index) in users" :key="user.user_id" :class="`tier tier--${user.tier}`">
+						<th scope="row" class="tier__index"><span :class="`tier__ball tier__ball--${user.tier}`" :style="`zIndex: ${index}`">{{ index + 1 }}</span></th>
 						<td><a href="#"
 							data-bs-toggle="modal"
 							data-bs-target="#hulaModalmatch"
 							v-on:click="currentMatch = user
 						">{{ user.user_first_name }} {{ user.user_last_name }}</a></td>
-						<td>{{ hasAllMandatorySkills(user) }}</td>
+						<td>{{ user.hasMandatory }}</td>
 						<td><span class="badge" v-for="match in user.matches" :key="match.skill_id">{{ match.skill_label }}</span></td>
-						<td>{{ tier(user) }}</td>
-						<td>{{ isAvailable(user) }}</td>
+						<td>{{ user.tier }}</td>
+						<td>{{ user.isAvailable }}</td>
 					</tr>
 				</tbody>
 			</table>
@@ -59,33 +59,37 @@
 		},
 		computed: {
 			users() {
-				const users = Object.values(this.matches.reduce((users, match) => {
-					const {
-						user_id,
-						user_first_name,
-						user_last_name,
-						user_is_hidden,
-						project_id,
-						...rest
-					} = match
-					if (user_id in users) {
-						users[user_id].matches.push(rest)
-					} else {
-						users[user_id] = {
+				if (this.matches.length) {
+					console.log("Running the users and using resources!")
+					this.getProjectSkills(this.matches[0].project_id)
+					const users = Object.values(this.matches.reduce((users, match) => {
+						const {
 							user_id,
 							user_first_name,
 							user_last_name,
 							user_is_hidden,
 							project_id,
-							matches: [rest],
+							...rest
+						} = match
+						if (user_id in users) {
+							users[user_id].matches.push(rest)
+						} else {
+							users[user_id] = {
+								user_id,
+								user_first_name,
+								user_last_name,
+								user_is_hidden,
+								project_id,
+								matches: [rest],
+							}
 						}
-					}
+						users[user_id].hasMandatory = this.hasAllMandatorySkills(users[user_id])
+						users[user_id].tier = this.tier(users[user_id])
+						users[user_id].isAvailable = this.isAvailable(users[user_id])
+						return users
+					}, {}))
 					return users
-				}, {}))
-				if (users.length) {
-					this.getProjectSkills(users[0].project_id)
 				}
-				return users
 			},
 		},
 		methods: {
@@ -105,13 +109,6 @@
 			hasAllMandatorySkills(user) {
 				return this.mandatorySkills.every(skill => {
 					return user.matches.some(match => {
-						if (user.user_first_name == "Mace") {
-							console.log("Windu")
-							console.log(
-								match.skill_label, skill.skill_label, match.user_index, match.required_index,
-								match.user_years, match.required_minyears, match.user_years, match.required_maxyears
-							)
-						}
 						return match.skill_label === skill.skill_label
 						&& match.user_index >= match.required_index
 						&& match.user_years >= match.required_minyears
