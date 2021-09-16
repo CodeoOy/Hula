b06c8aceb04b2c069c17889d959642d64d8042fb
Pekka Sintonen <sintopek@gmail.com>
2021-09-16 07:38:48 +0300

    sorting skills per mandatory flag

--- src/repositories/projectskills_repository.rs
+++ src/repositories/projectskills_repository.rs
@@ -7,12 +7,12 @@ use crate::models::projects::Project;
 use crate::models::projectskills::ProjectSkill;
 
 pub fn find_by_projects(projects: &Vec<Project>, pool: &web::Data<Pool>) -> Result<Vec<Vec<ProjectSkill>>, Error> {
-	use crate::schema::projectskills::dsl::skill_label;
+	use crate::schema::projectskills::dsl::{is_mandatory, skill_label};
 
 	let conn: &PgConnection = &pool.get().unwrap();
 
 	let skills = ProjectSkill::belonging_to(projects)
-		.order(skill_label.asc())
+		.order((is_mandatory.desc(), skill_label.asc()))
 		.load::<ProjectSkill>(conn)?
 		.grouped_by(&projects);
 
