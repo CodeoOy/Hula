7abb4c925560c97f87c1d1ea8b2215537eb64bcd
Tuukka Toijonen <8547233+taqtaq@users.noreply.github.com>
2021-09-22 11:38:49 +0300

    Tweak project matches layout

--- app/src/components/VMatchesForProject.vue
+++ app/src/components/VMatchesForProject.vue
@@ -3,27 +3,32 @@
 		<table class="table table-striped mb-0" :class='$colorScheme.table'>
 			<thead>
 				<tr>
-					<th scope="col"></th>
+					<th scope="col" class='d-none d-sm-block'>
+						<div class='visually-hidden'>Match order</div>
+					</th>
 					<th scope="col">Name</th>
-					<th scope="col" class='text-center'>Mandatory skills</th>
-					<th scope="col">Matched skills</th>
-					<th scope="col" class='text-center'>Available</th>
+					<th scope="col">Skills</th>
+					<th scope="col" class='text-center'>
+						<div class='d-none d-sm-block'>Mandatory</div>
+						<i class='d-sm-none bi-tools' title='Mandatory skills'></i>
+					</th>
+					<th scope="col" class='text-center'>
+						<div class='d-none d-sm-block'>Available</div>
+						<i class='d-sm-none bi-clock-fill' title='Available'></i>
+					</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr v-for="(user, index) in users" :key="user.user_id" :class="`tier tier-${user.tier}`">
-					<th scope="row"><div class="tier-circle shadow-sm" :style='{ zIndex: users.length - index }'>{{ index + 1 }}</div></th>
+					<th scope="row" class='d-none d-sm-table-cell'><div class="tier-circle shadow-sm" :style='{ zIndex: users.length - index }'>{{ index + 1 }}</div></th>
 					<td>
 						<button v-on:click='showMatch(user)' class='btn btn-unstyled'>
 							{{ user.user_first_name }} {{ user.user_last_name }} 
 						</button>
 						<i v-if='user.user_favorite' class='bi-star-fill text-yellow ms-2 float-end'></i>
 					</td>
-					<td class='text-center'>
-						<i v-if='user.hasMandatory' class='bi-check-lg'></i>
-					</td>
 					<td class='align-middle'>
-						<div class='hstack gap-2'>
+						<div class='hstack gap-2 flex-wrap'>
 							<VSkillBadge
 								v-for="skill in user.skills"
 								:key="skill.skill_id"
@@ -34,7 +39,10 @@
 						</div>
 					</td>
 					<td class='text-center'>
-						<i v-if='user.isAvailable' class='bi-check-lg'></i>
+						<i v-if='user.hasMandatory' class='bi-check-lg' title='Mandatory skills'></i>
+					</td>
+					<td class='text-center'>
+						<i v-if='user.isAvailable' class='bi-check-lg' title='Available'></i>
 					</td>
 				</tr>
 			</tbody>
