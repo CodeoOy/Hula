97fd84f794984f0bbc43f1f8f6d0fd376d7deb96
Antti Kalliokoski <antti.kalliokoski@codeo.fi>
2021-11-16 15:17:26 +0200

    Add initial privacy policy

--- app/src/views/Gdpr.vue
+++ app/src/views/Gdpr.vue
@@ -5,37 +5,112 @@
 				<h1 class="h1">Privacy policy</h1>
 				<br />
 
-				<h3 class="h3">Registry administrator information</h3>
-				- Name of the company<br />
-				- Contact information<br />
-				- Contact information of data protection officer
-				<br />
+				<h3 class="h3">A. Introduction</h3>
+				<p>This policy explains what we do with your personal information.</p>
+				
+				<h3 class="h3">B. How we collect your personal data</h3>
+				<p>The following types of personal information may be collected, stored, and used:</p>
+				<p>1. Information that you enter when you register with our website, such as
+				your name and email address.</p>
+				<p>2. Information that you enter when you modify a profile on our website. For example skills your skills
+				that will be mapped against the available projects.</p>
+				<p>3. Any other personal information you send to us.</p>
 
-				<h3 class="h3">Information collected</h3>
-				- What information organization intends to process and how?<br />
-				- The purpose for which organization processes personal information<br />
-				- How long do keep your personal data?
-				<br />
+				<h3 class="h3">C. Using Personal Information</h3>
+				<p>Personal information submitted to us through our website will be used for the
+				purposes specified in this policy or on the relevant pages of the website. We may
+				use your personal information for the following:</p>
+				<p>1. Administering our website and business</p>
+				<p>2. Personalizing our website for you</p>
+				<p>3. Enabling your use of the services available on our website</p>
+				<p>4. Sending you email notifications you have specifically requested.</p>
+				<p>5. Dealing with inquiries and complaints made by or about you relating to
+				our website</p>
+				<p>6. Keeping our website secure and prevent fraud.</p>
+				<p>7. Verifying compliance with the terms and conditions governing the use of
+				our website.</p>
+				<p>8. Other uses.</p>
+				<p>If you submit personal information for publication on our website, we will publish
+				and otherwise use that information in accordance with the license you grant us.</p>
+				<p>We will not, without your expressed consent, supply your personal information to
+				any third party for their or any other third party’s direct marketing.</p>
 
-				<h3 class="h3">Sharing of personal data</h3>
-				- Who do share personal data with?<br />
-				- Which other organization or person can get the information?<br />
-				- Whether personal data is transferred outside the EU?
-				<br />
+				<h3 class="h3">D. Disclosing personal information</h3>
+				<p>We may disclose your personal information to any of our employees, officers,
+				insurers, professional advisers, agents, suppliers, or subcontractors as reasonably
+				necessary for the purposes set out in this policy.</p>
+				<p>We may disclose your personal information to any member of our group of
+				companies (this means our subsidiaries, our ultimate holding company and all its
+				subsidiaries) as reasonably necessary for the purposes set out in this policy.</p>
+				<p>We may disclose your personal information:</p>
+				<p>1. to the extent that we are required to do so by law;</p>
+				<p>2. in connection with any ongoing or prospective legal proceedings;</p>
+				<p>3. in order to establish, exercise, or defend our legal rights (including providing
+				information to others for the purposes of fraud prevention and reducing
+				credit risk);</p>
+				<p>4. to the purchaser (or prospective purchaser) of any business or asset that we
+				are (or are contemplating) selling; and</p>
+				<p>5. to any person who we reasonably believe may apply to a court or other
+				competent authority for disclosure of that personal information where, in our
+				reasonable opinion, such court or authority would be reasonably likely to
+				order disclosure of that personal information.</p>
+				<p>Except as provided in this policy, we will not provide your personal information to
+				third parties.</p>
 
-				<h3 class="h3">Rights of the data subject</h3>
-				- The Right to Information<br />
-				- The Right of Access<br />
-				- The Right to Rectification<br />
-				- The Right to Erasure<br />
-				- The Right to Restriction of Processing<br />
-				- The Right to Data Portability<br />
-				- The Right to Object<br />
-				- The Right to Avoid Automated Decision-Making<br />
-				<br />
+				<h3 class="h3">E. International data transfers</h3>
+				<p>1. Information that we collect may be stored, processed in, and transferred
+				between any of the countries in which we operate in order to enable us to
+				use the information in accordance with this policy.</p>
+				<p>2. Personal information that you publish on our website or submit for
+				publication on our website may be available, via the internet, around the
+				world. We cannot prevent the use or misuse of such information by others.</p>
+				<p>3. You expressly agree to the transfers of personal information described in
+				this Section E.</p>
+		
+				<h3 class="h3">F. Retaining personal information</h3>
+				<p>1. This Section F sets out our data retention policies and procedure, which are
+				designed to help ensure that we comply with our legal obligations regarding
+				the retention and deletion of personal information.</p>
+				<p>2. Personal information that we process for any purpose or purposes shall not
+				be kept for longer than is necessary for that purpose or those purposes.</p>
+				<p>4. Notwithstanding the other provisions of this Section F, we will retain
+				documents (including electronic documents) containing personal data:</p>
+				<p>a. to the extent that we are required to do so by law;</p>
+				<p>b. if we believe that the documents may be relevant to any ongoing or
+				prospective legal proceedings; and</p>
+				<p>c. in order to establish, exercise, or defend our legal rights (including
+				providing information to others for the purposes of fraud prevention
+				and reducing credit risk).</p>
+		
+				<h3 class="h3">G. Security of your personal information</h3>
+				<p>1. We will take reasonable technical and organizational precautions to prevent
+				the loss, misuse, or alteration of your personal information.</p>
+				<p>2. We will store all the personal information you provide on our secure servers.</p>
+				<p>3. You are responsible for keeping the password you use for accessing our
+				website confidential; we will not ask you for your password (except when
+				you log in to our website).</p>
 
-				<h3 class="h3">Cookies</h3>
-				Which cookies the company uses?
+				<h3 class="h3">H. Amendments</h3>
+				<p>We may update this policy from time to time by publishing a new version on our
+				website. You should check this page occasionally to ensure you understand any
+				changes to this policy. We may notify you of changes to this policy by email.</p>
+
+				<h3 class="h3">I. Your rights</h3>
+				<p>You may instruct us to provide you with any personal information we hold about
+				you.</p>
+				<p>We may withhold personal information that you request to the extent permitted by
+				law.</p>
+				<p>You may instruct us at any time not to process your personal information for
+				marketing purposes.</p>
+
+				<h3 class="h3">J. Updating information</h3>
+				<p>Please let us know if the personal information that we hold about you needs to be
+				corrected or updated.</p>
+
+				<h3 class="h3">K. Cookies</h3>
+				<p>Our website uses a persistent cookie ('auth') for user authentication and authorization.
+				The cookie is required to be able to sign-in and use our website. 
+				</p>
 			</div>
 		</div>
 	</div>
